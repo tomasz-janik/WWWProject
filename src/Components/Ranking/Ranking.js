bdc303fee04086ae3e1d53b0b267d21d8b46fba8
Tomasz Janik <tomasz.janik98@gmail.com>
2019-12-01 13:26:21 +0100

    Added filtering on table

--- src/Components/Ranking/Ranking.js
+++ src/Components/Ranking/Ranking.js
@@ -9,7 +9,11 @@ class Ranking extends Component {
     state = {
         isLoading: true,
         data: [],
+        displayedData: [],
         error: false,
+        idFilterValue: "",
+        nameFilterValue: "",
+        ratingFilterValue: ""
     }
 
     componentDidMount() {
@@ -23,25 +27,25 @@ class Ranking extends Component {
 
     loadData = () => {
         fetch('http://localhost:8081/data2')
-        .then(response => response.json())
-        .then(response => {
-            if (this._isMounted) {
+            .then(response => response.json())
+            .then(response => {
+                if (this._isMounted) {
+                    this.setState(
+                        {
+                            isLoading: false,
+                            data: this.state.data.concat(response),
+                            displayedData: this.state.data.concat(response)
+                        });
+                }
+            })
+            .catch(err => {
+                console.log(err);
                 this.setState(
                     {
                         isLoading: false,
-                        data: this.state.data.concat(response),
-                    });
-                    console.log(this.state.data)
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            this.setState(
-                {
-                    isLoading: false,
-                }
-            )
-        });
+                    }
+                )
+            });
     }
 
     renderTableHeader() {
@@ -54,8 +58,16 @@ class Ranking extends Component {
     }
 
     renderTableData() {
-        return this.state.data.map((entry, index) => {
-            console.log(entry)
+        if (this.state.displayedData.length === 0){
+            return (
+                <tr>
+                    <td colspan="3">
+                        None of avalible data matches your request!
+                    </td>
+                </tr>
+            )
+        }
+        return this.state.displayedData.map((entry, index) => {
             return (
                 <tr key={index}>
                     <td>{entry.id}</td>
@@ -66,20 +78,76 @@ class Ranking extends Component {
         })
     }
 
+    renderTableFilters = () => {
+        return (
+            <tr>
+                <td>
+                    <input type="text" value={this.state.idFilterValue} onChange={this.handleIdFilterChange} />
+                </td>
+                <td>
+                    <input type="text" value={this.state.nameFilterValue} onChange={this.handleNameFilterChange} />
+                </td>
+                <td>
+                    <input type="text" value={this.state.ratingFilterValue} onChange={this.handleRatingFilterChange} />
+                </td>
+            </tr>
+        )
+    }
+
+    handleIdFilterChange = (event) => {
+        this.setState(
+            {
+                idFilterValue: event.target.value,
+                displayedData: this.state.data.filter((entry) => {
+                    return (entry.id + '').includes(event.target.value)
+                        && entry.name.includes(this.state.nameFilterValue)
+                        && entry.rating.includes(this.state.ratingFilterValue);
+                })
+            }
+        );
+    }
+
+    handleNameFilterChange = (event) => {
+        this.setState(
+            {
+                nameFilterValue: event.target.value,
+                displayedData: this.state.data.filter((entry) => {
+                    return (entry.id + '').includes(this.state.idFilterValue)
+                        && entry.name.includes(event.target.value)
+                        && entry.rating.includes(this.state.ratingFilterValue);
+                })
+            }
+        );
+    }
+
+    handleRatingFilterChange = (event) => {
+        this.setState(
+            {
+                ratingFilterValue: event.target.value,
+                displayedData: this.state.data.filter((entry) => {
+                    return (entry.id + '').includes(this.state.idFilterValue)
+                        && entry.name.includes(this.state.nameFilterValue)
+                        && entry.rating.includes(event.target.value);
+                })
+            }
+        );
+    }
+
     render() {
         return (
             <Card>
-                {!this.state.isLoading &&
-                    <div className='table_border'>
-                        <h1 className='table_name'>Ranking</h1>
+                <div className='table_border'>
+                    <h1 className='table_name'>Ranking</h1>
                         <table className='table'>
                             <tbody>
-                                <tr>{this.renderTableHeader()}</tr>
+                                <tr>
+                                    {this.renderTableHeader()}
+                                </tr>
+                                {this.renderTableFilters()}
                                 {this.renderTableData()}
                             </tbody>
                         </table>
-                    </div>
-                }
+                </div>
             </Card>
         );
     }
